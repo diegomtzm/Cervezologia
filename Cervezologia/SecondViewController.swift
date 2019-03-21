df3439c19800e74ebe809cbc75cac6fb052d2570
Diego Martinez <diegomtz1998@hotmail.com>
2019-03-21 10:47:29 -0600

    Initial project setup

---
+++ Cervezologia/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Cervezologia
+//
+//  Created by Diego Martinez on 3/21/19.
+//  Copyright © 2019 Diego Martinez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
